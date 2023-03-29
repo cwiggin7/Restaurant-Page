bec6dbdaf76f188236cfdb44df71fc8609809d94
cwiggin7 <cwiggin7@ksu.edu>
2023-03-28 23:48:05 -0500

    Update image width in #owner-section to be responsive to viewport changes

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,96 @@
-(()=>{"use strict";(()=>{const n=document.querySelector("#content"),i=document.createElement("div");i.classList.add("tab-content"),i.innerHTML='<div id="top-nav">\n    <ul>\n        <li><a href="#">Home</a></li>\n        <li><a href="#">About</a></li>\n        <li><a href="#">Menu</a></li>\n        <li><a href="#">Order Online</a></li>\n    </ul>\n</div>\n<div id="heading-section">\n    <h1>Trattoria Trussardi<br><span>Italian Restaurant</span></h1>\n</div>\n<div id="owner-section">\n    <div id="owner-container">\n        <h3>Our Founder and Owner</h3>\n        <div>Meet Tonio Trussardi, the benevolent owner and chef of Trattoria Trussardi. Tonio\'s dishes are not only delicious but also possess the power to heal, thanks to his unwavering dedication to using only the freshest and healthiest ingredients. Come and experience the warmth and charm of Tonio\'s hospitality at Trattoria Trussardi.</div>\n    </div>              \n    <img src="./imgs/Tonio-Trussardi.png" alt="Tonio Trussardi" id="tonio-image">\n</div>',n.appendChild(i)})()})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/home.js":
+/*!*********************!*\
+  !*** ./src/home.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"renderHomePage\": () => (/* binding */ renderHomePage)\n/* harmony export */ });\nconst renderHomePage = (() => {\r\n    //container\r\n    const contentContainer = document.querySelector(\"#content\");\r\n  \r\n    const home = document.createElement(\"div\");\r\n    home.classList.add(\"tab-content\")\r\n    home.innerHTML = `<div id=\"top-nav\">\r\n    <ul>\r\n        <li><a href=\"#\">Home</a></li>\r\n        <li><a href=\"#\">About</a></li>\r\n        <li><a href=\"#\">Menu</a></li>\r\n        <li><a href=\"#\">Order Online</a></li>\r\n    </ul>\r\n</div>\r\n<div id=\"heading-section\">\r\n    <h1>Trattoria Trussardi<br><span>Italian Restaurant</span></h1>\r\n</div>\r\n<div id=\"owner-section\">\r\n    <div id=\"owner-container\">\r\n        <h3>Our Founder and Owner</h3>\r\n        <div>Meet Tonio Trussardi, the benevolent owner and chef of Trattoria Trussardi. Tonio's dishes are not only delicious but also possess the power to heal, thanks to his unwavering dedication to using only the freshest and healthiest ingredients. Come and experience the warmth and charm of Tonio's hospitality at Trattoria Trussardi.</div>\r\n    </div>              \r\n    <img src=\"./imgs/Tonio-Trussardi.png\" alt=\"Tonio Trussardi\" id=\"tonio-image\">\r\n</div>`;\r\n  \r\n    contentContainer.appendChild(home);\r\n})();\r\n\r\n\r\n\n\n//# sourceURL=webpack://restaurant-page/./src/home.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home */ \"./src/home.js\");\n\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
